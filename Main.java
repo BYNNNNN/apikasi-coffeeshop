d008ba6d195a74f9b7c6cd057a76642f1591c67c
Suryo Bintoro <101824105+BYNNNNN@users.noreply.github.com>
2023-01-08 11:01:02 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,25 +8,30 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int pilih_menu;
         do {
-            System.out.println("\n\n_____________________________________________________");
+            System.out.println("\n\n-----------------------------------------------------");
             System.out.println("|    PROGRAM KASIR PEMESANAN PADA COFFE SHOP        |");
-            System.out.println("_____________________________________________________");
+            System.out.println("|                 BORIA SUKA-3SUKA                   |");
+            System.out.println("-----------------------------------------------------");
             System.out.println("1. PEMBELIAN ");
-            System.out.println("2. STRUK BELANJA");
-            System.out.println("3. KELUAR ");
-            System.out.printf("   MASUKAN PILIHAN ANDA         = ");
+            System.out.println("2. DAFTAR PEMBELIAN");
+            System.out.println("3. STRUK PEMBELIAN");
+            System.out.println("4. KELUAR ");
+            System.out.printf("   MASUKAN PILIHAN ANDA   = ");
             pilih_menu = input.nextInt();
             if (pilih_menu==1){
-                cs.proses();
+                cs.pembelian();
             }
-//            else if (pilih_menu==2) {
-//                cs.struk();
-//            }
-        else if (pilih_menu==3){
+            else if (pilih_menu==2) {
+                cs.daftar();
+            }
+            else if (pilih_menu==3){
+                cs.struk();
+            }
+            else if (pilih_menu==4){
                 System.out.println("TERIMAKASIH !!!!");
             }else {
                 System.out.println("DATA YANG ANDA MASUKAN TIDAK VALID !!!");
             }
-        }while (pilih_menu!=3);
+        }while (pilih_menu!=4);
     }
 }
